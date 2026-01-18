847c743191d737f07c523264d6e38378088151b7
Tony-exe <antony.boss2003@gmail.com>
2026-01-18 18:37:11 +0100

    Api get

--- Mini_flight_tracker/flight_info.h
+++ Mini_flight_tracker/flight_info.h
@@ -10,22 +10,3 @@ typedef struct info{
 typedef struct {
 
 }httpResponse;
-
-// Callback interno (private)
-static size_t write_callback(void *contents, size_t size, size_t nmemb, void *userp) {
-    size_t realsize = size * nmemb;
-    HttpResponse *mem = (HttpResponse *)userp;
-    
-    char *ptr = realloc(mem->data, mem->size + realsize + 1);
-    if(!ptr) {
-        fprintf(stderr, "not enough memory\n");
-        return 0;
-    }
-    
-    mem->data = ptr;
-    memcpy(&(mem->data[mem->size]), contents, realsize);
-    mem->size += realsize;
-    mem->data[mem->size] = 0;
-    
-    return realsize;
-}
